18d3d4a43f76d6ba2d58ac716af5142fc4c84e53
pml16 <pml16@hydra.ml.tu-berlin.de>
2024-11-26 18:22:27 +0100

    initial commit

---
+++ train.py
@@ -0,0 +1,126 @@
+import argparse
+import torch
+import os
+
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+import torch.nn.functional as F
+
+from pathlib import Path
+from datasets.camelyon import load_data
+from models.TransMILBaseline import TransMILBaseline
+from torch.utils.data import DataLoader
+from visualization import plot_stats
+
+def make_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--split', type=str)
+    parser.add_argument('--num_epochs', default=10, type=int)
+    # parser.add_argument('--config')
+    args = parser.parse_args()
+    return args
+
+def common_compute(model, batch, criterion):
+    features, labels = batch
+    features, labels = features.to(DEVICE), labels.to(DEVICE)
+    logits = model.forward(features)
+    loss = criterion(logits, labels)
+    return logits, loss, labels
+
+def training_step(model, batch, optimizer, criterion):
+    logits, loss, _ = common_compute(model, batch, criterion)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+    return loss
+
+def test_step(model, batch, criterion):
+    logits, loss, y = common_compute(model, batch, criterion)
+    _, preds = torch.max(logits.detach(), axis=1) # [B, 1]
+    correct_preds = (preds == y).sum().item()
+    return loss, y.shape[0], correct_preds
+
+def val_step(model, batch, criterion):
+    logits, loss, y = common_compute(model, batch, criterion)
+    _, preds = torch.max(logits.detach(), axis=1) # [B, 1]
+    correct_preds = (preds == y).sum().item()
+    return loss, y.shape[0], correct_preds
+
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+def main(split: str, num_epochs: int):
+    train_dataset, test_dataset, val_dataset = load_data(split_file=split, base_dir='/mnt/')
+
+    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
+
+    model = TransMILBaseline(new_num_features=384, n_heads=4, num_classes=2, device=DEVICE)
+    model.to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
+    criterion = nn.CrossEntropyLoss()
+
+    batch_size = 1
+    epochs = num_epochs
+
+    train_epoch_loss = []
+    val_epoch_loss = []
+    val_epoch_acc = []
+
+    for epoch in range(epochs):
+        loss_total = []
+        correct = 0
+        total = 0
+        print(f"epoch {epoch+1} out of {epochs}")
+        for batch in train_dataloader:
+            model.train()
+            loss = training_step(model, batch, optimizer, criterion)
+            loss_total.append(loss.detach().item())
+        avg_epoch_loss = sum(loss_total)/len(train_dataloader)
+        train_epoch_loss.append(avg_epoch_loss)
+        
+        print(f"avg loss after epoch {epoch+1} / {epochs} is: {avg_epoch_loss}")
+        loss_total.clear()
+
+        with torch.no_grad():
+            for batch in val_dataloader:
+                model.eval()
+                loss, batch_size, correct_preds = val_step(model, batch, criterion)
+                loss_total.append(loss.detach().item())
+                total += batch_size
+                correct += correct_preds
+
+        avg_epoch_loss = sum(loss_total)/len(val_dataloader)
+        acc = correct / total * 100
+        
+        val_epoch_loss.append(avg_epoch_loss)
+        val_epoch_acc.append(acc)
+        print(f"Validation accuracy after epoch {epoch+1} / {epochs} is: {acc}")
+
+    plot_stats(epochs, train_epoch_loss, "Train loss", plot_type = "loss", save=True, save_path="/home/pml16/")
+    plot_stats(epochs, val_epoch_loss, "Val loss", plot_type = "loss", save=True, save_path="/home/pml16/")
+    plot_stats(epochs, val_epoch_acc, "Val accuracy", plot_type = "acc", save=True, save_path="/home/pml16/")
+
+    loss_total = []
+    with torch.no_grad():
+        for batch in test_dataloader:
+            correct, total = 0, 0
+            model.eval()
+            loss, batch_size, correct_preds = test_step(model, batch, criterion)
+            loss_total.append(loss.detach().item())
+            total += batch_size
+            correct += correct_preds
+        avg_test_loss = sum(loss_total)/len(test_dataloader)
+        acc = correct / total * 100
+        print(f"avg loss after test is: {avg_test_loss}")
+        print(f"Test accuracy after {epochs} epochs training is: {acc}")
+
+
+if __name__ == "__main__":
+    args = make_parse()
+    split = args.split
+    num_epochs = args.num_epochs
+    main(split, num_epochs)
+
---
+++ visualization.py
@@ -0,0 +1,130 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+from typing import List
+
+def plot_stats(epochs, y_vals, plot_name, plot_type = ["loss", "acc"], save=False, save_path=""):
+    if plot_type not in ['loss', 'acc']:
+        raise ValueError(f"Type {plot_type} is not valid for plotting")
+    x = list(range(1,epochs+1))
+    y = y_vals
+
+    plt.plot(x, y)
+    plt.title(plot_name)
+    plt.xlabel("Epoch")
+    plt.ylabel(plot_type)
+
+    if save:
+        save_path = Path(save_path, f"{plot_type}_{plot_name}_{epochs}_epochs.png")
+        plt.savefig(save_path, bbox_inches="tight", dpi=300)  # High resolution and tight layout
+        print(f"Plot saved to {save_path}")
+    plt.show()
+    plt.clf()
+
+def visualize_slide_with_patches(slide_id: str = None, outfile: Path = None, base_dir="/mnt/"):
+    # Prepare path names
+    if slide_id:
+        slide_id = slide_id
+        # WSI filepath
+        wsi_path = Path(base_dir, "slides", f"{slide_id}.tif")
+        # WSI's patches datafile - containing information about their position in the WSI
+        csv_path = Path(base_dir, "patches", "20x", slide_id, "metadata", "df.csv")
+        # File containing WSI's label info
+        label_path = Path(base_dir, "splits/camelyon16_tumor_85_15_orig_0.csv")
+    else:
+        raise ValueError("No slide_id has been given to the function")
+
+    df_labels = pd.read_csv(label_path)
+    wsi_label = df_labels['label'].loc[df_labels['case_id'] == slide_id].tolist()[0]
+
+    # Assign string labels instead of int
+    if wsi_label == 0:
+        label = "No Tumor"
+    elif wsi_label == 1:
+        label = "Tumor"
+
+    # Load the WSI
+    slide = openslide.OpenSlide(wsi_path)
+
+    # Adjusting resolution level for visualization
+    level = slide.level_count - 3
+    wsi_dimensions = slide.level_dimensions[level]
+    wsi_thumbnail = slide.read_region(location=(0, 0), level=level, size=wsi_dimensions)
+    wsi_thumbnail = wsi_thumbnail.convert("RGB")
+
+    # Downsampling factor for the selected level
+    downsample_factor = slide.level_downsamples[level]
+
+    # Check if WSI is vertical (rotate if necessary)
+    rotated = False
+    if wsi_dimensions[1] > wsi_dimensions[0]:
+        print("Rotating WSI to horizontal position.")
+        wsi_thumbnail = wsi_thumbnail.rotate(-90, expand=True)
+        rotated = True
+
+    # Load patch coordinates from the CSV
+    df = pd.read_csv(csv_path)
+
+    # Randomly sample N patches and put their ids on the list
+    N=5
+    selected_patches = df.sample(n=N).reset_index()
+    selected_patches_ids = selected_patches['path'].tolist()
+
+    # Plot the rotated WSI
+    plt.figure(figsize=(12, 12))
+    plt.imshow(np.array(wsi_thumbnail))
+    plt.axis("off")
+
+    for i, row in selected_patches.iterrows():
+        # Parse position_abs (convert string "(x, y)" to tuple)
+        position_abs = eval(row["position_abs"])  # Example: "(512, 10240)" -> (512, 10240)
+        x_abs, y_abs = position_abs
+        x_scaled = x_abs / downsample_factor
+        y_scaled = y_abs / downsample_factor
+
+        # Get patch size
+        patch_size_abs = row["patch_size_abs"]
+        patch_size_scaled = patch_size_abs / downsample_factor
+
+        if rotated:
+            # Adjust coordinates for a 90° clockwise rotation
+            x_scaled_new = wsi_dimensions[1] - y_scaled - patch_size_scaled
+            y_scaled_new = x_scaled
+            x_scaled, y_scaled = x_scaled_new, y_scaled_new
+
+        # Draw the rectangle
+        rect = patches.Rectangle((x_scaled, y_scaled), patch_size_scaled, patch_size_scaled,
+                                    linewidth=2, edgecolor='red', facecolor='none')
+        plt.gca().add_patch(rect)
+        plt.text(x_scaled, y_scaled - 5, f"{i+1}", color='black', fontsize=10)
+
+    plt.title(f"Rotated WSI with Selected Patches Highlighted - slide {slide_id}, label: {label}")
+    if outfile:
+        plt.savefig(outfile, dpi=300, bbox_inches='tight')
+    plt.show()
+
+    # Show single patches randomly chosen earlier
+    show_patches(patch_ids=selected_patches_ids, rows=1, cols=5, outfile=None)
+
+
+def show_patches(patch_ids: List[str], rows: int, cols: int, outfile: Path = None):
+    if len(patch_ids != rows*cols):
+        raise ValueError(f"{len(patch_ids)} patches cannot be ploted in {rows} rows and {cols} columns grid")
+    imgs = patch_ids
+
+    figure, axes = plt.subplots(rows, cols, figsize=(7,7))
+    axes = axes.flatten()
+    # Add single patches to the plot
+    for idx, ax in enumerate(axes):
+        if idx < len(imgs):
+            img = plt.imread(Path("/mnt/patches/20x/", imgs[idx]))
+            ax.set_title(idx+1, fontsize='8')
+            ax.imshow(img)
+            ax.axis('off')
+    plt.tight_layout()
+    if outfile:
+        plt.savefig(outfile, dpi=300, bbox_inches='tight')
+    plt.show()
